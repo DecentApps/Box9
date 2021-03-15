ad09351fc7cf27b1bc31388f15cac4ed34f9bf02
ydra9 <ydra.nine@gmail.com>
2021-03-15 14:01:28 +0700

    adding SafeMath library

---
+++ contracts/Box9.sol
@@ -0,0 +1,10 @@
+/* https://www.apache.org/licenses/LICENSE-2.0 */
+
+pragma solidity 0.4.21
+
+import "./IEBox9.sol";
+import "./SafeMath.sol";
+
+contract Box9 is IBox9 {
+    
+}
---
+++ contracts/Ibox9.sol
@@ -0,0 +1,7 @@
+/* https://www.apache.org/licenses/LICENSE-2.0 */
+
+pragma solidity 0.4.21
+
+interface Ibox9 {
+    
+}
